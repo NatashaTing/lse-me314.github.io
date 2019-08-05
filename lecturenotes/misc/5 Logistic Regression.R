78086eb789ff6db57c5d23d6d7da34fb190f8385
NatashaTing <natasha@zept.ca>
2019-08-05 14:16:56 +0100

    add my notes

--- lecturenotes/misc/5 Logistic Regression.R
+++ lecturenotes/misc/5 Logistic Regression.R
@@ -1,5 +1,6 @@
 #################################
 # code block #1
+# TODO compare this with the lab section of Chapter05
 
 # Helper packages
 library(dplyr)     # for data wrangling
@@ -8,10 +9,11 @@ library(rsample)   # for data splitting
 
 # Modeling packages
 library(caret)     # for logistic regression modeling
+# instructor "can make your life in ML much easier"
+# allows you to train your model and simplify 
 
 # Model interpretability packages
 library(vip)       # variable importance
-
 
 #################################
 # code block #2
@@ -31,10 +33,10 @@ churn_test  <- testing(churn_split)
 model1 <- glm(Attrition ~ MonthlyIncome, family = "binomial", data = churn_train)
 model2 <- glm(Attrition ~ OverTime, family = "binomial", data = churn_train)
 
-
 #################################
 # code block #4
 
+# tidy is from library broom. turn the output into a tidy tibble. 
 tidy(model1)
 ## # A tibble: 2 x 5
 ##   term           estimate std.error statistic  p.value
@@ -64,6 +66,7 @@ exp(coef(model2))
 exp(coef(model1)) / (1 + exp(coef(model1)))
 ##   (Intercept) MonthlyIncome 
 ##     0.2721929     0.4999714
+# NT: if you don't specify the x value, then you're just using the average of the X's ($\bar{x}$)
 exp(coef(model2)) / (1 + exp(coef(model2)))
 ## (Intercept) OverTimeYes 
 ##   0.1054054   0.7871156
@@ -87,6 +90,7 @@ confint(model2)
 
 model3 <- glm(Attrition ~ MonthlyIncome + OverTime, family = "binomial", data = churn_train)
 tidy(model3)
+# NT: this one outputs  the Z-statistic on the 4th column 
 ## # A tibble: 3 x 5
 ##   term           estimate std.error statistic  p.value
 ##   <chr>             <dbl>     <dbl>     <dbl>    <dbl>
@@ -98,15 +102,20 @@ tidy(model3)
 #################################
 # code block #8
 
+install.packages("e1071")
+library(e1071)
+
 set.seed(123)
 cv_model1 <- train(
   Attrition ~ MonthlyIncome, 
   data = churn_train, 
   method = "glm",
   family = "binomial",
-  trControl = trainControl(method = "cv", number = 10)
+  trControl = trainControl(method = "cv", number = 10)  # this is using cross validation
 )
 
+
+# CV is the way to estimate your model from the training set that you have
 set.seed(123)
 cv_model2 <- train(
   Attrition ~ MonthlyIncome + OverTime, 
@@ -140,6 +149,19 @@ summary(
 ## model2 0.8349515 0.8353482 0.8365385 0.8388542 0.8431373 0.8446602    0
 ## model3 0.8058252 0.8389423 0.8585570 0.8631848 0.8949172 0.9134615    0
 
+# NT: there is a spread because for each model_i, there are 10 different models from cross validation
+
+###############################
+
+summary(
+  resamples(
+    list(
+      model1 = model1, 
+      model2 = model2, 
+      model3 = model3
+    )
+  )
+)$statistics$Accuracy
 
 #################################
 # code block #9
@@ -148,6 +170,7 @@ summary(
 pred_class <- predict(cv_model3, churn_train)
 
 # create confusion matrix
+# this comes from the `caret` package
 confusionMatrix(
   data = relevel(pred_class, ref = "Yes"), 
   reference = relevel(churn_train$Attrition, ref = "Yes")
@@ -181,6 +204,21 @@ confusionMatrix(
 ## 
 
 
+# NT: No information rate is the Null error rate . Is it good to be big or 
+# TODO: STUDY THIS OUTPUT
+# Kappa is bounded between [0,1]
+
+
+#################
+# let's change the language to ML-preferred things
+
+confusionMatrix( mode = "prec_recall",
+  data = relevel(pred_class, ref = "Yes"), 
+  reference = relevel(churn_train$Attrition, ref = "Yes")
+)
+
+# TODO Study this too.....
+# `precision` picks up one thing, `recall` picks up F1
 #################################
 # code block #10
 
